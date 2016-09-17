7ace0130a38b0ec894cd63d6b3a51bdd81e42aad
Yury <permenko@gmail.com>
2016-09-18 00:42:29 +0300

    [task2] add default architecture
    remove unused code

--- app/src/main/java/com/recentgames/api/GiantBombService.java
+++ app/src/main/java/com/recentgames/api/GiantBombService.java
@@ -1,10 +1,4 @@
 package com.recentgames.api;
 
-import okhttp3.ResponseBody;
-import retrofit2.http.GET;
-
 public interface GiantBombService {
-
-    @GET("games")
-    ResponseBody getGames();
 }
