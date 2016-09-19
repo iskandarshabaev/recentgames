b67e9d326b2b11b0e72001a0a01044753498a455
Konstantin Rodionov <mail.rodionovk@gmail.com>
2016-09-19 17:11:18 +0300

    realize SearchFragment

--- app/src/main/java/com/recentgames/repository/DefaultGiantBombRepository.java
+++ app/src/main/java/com/recentgames/repository/DefaultGiantBombRepository.java
@@ -1,12 +1,16 @@
 package com.recentgames.repository;
 
+import com.recentgames.api.ApiFactory;
+import com.recentgames.model.QueryParams;
 import com.recentgames.model.content.GameDescription;
 import com.recentgames.model.content.GamePreview;
 import com.recentgames.model.content.ReviewDescription;
+import com.recentgames.model.response.GiantBombResponse;
 
 import java.util.List;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 public class DefaultGiantBombRepository implements GiantBombRepository {
 
@@ -21,8 +25,10 @@ public class DefaultGiantBombRepository implements GiantBombRepository {
     }
 
     @Override
-    public Observable<List<GamePreview>> search() {
-        return Observable.empty();
+    public Observable<List<GamePreview>> search(String name) {
+        return ApiFactory.getGiantBombService()
+                .search(name, QueryParams.GAMES_FILED_LIST,20,0)
+                .map(GiantBombResponse::getResults);
     }
 
     @Override
