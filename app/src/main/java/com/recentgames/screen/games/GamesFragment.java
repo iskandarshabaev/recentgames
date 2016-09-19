ca5aa13da6f586403b4547250c601d717751b89a
Home <Home>
2016-09-19 21:14:16 +0300

    import

--- app/src/main/java/com/recentgames/screen/games/GamesFragment.java
+++ app/src/main/java/com/recentgames/screen/games/GamesFragment.java
@@ -1,6 +1,7 @@
 package com.recentgames.screen.games;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
