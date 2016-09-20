9083975e0718bfedbbf75fd2b9519d99d7762113
Konstantin Rodionov <mail.rodionovk@gmail.com>
2016-09-20 10:51:40 +0300

    optimize by conversation pullrequest task14

--- app/src/main/java/com/recentgames/screen/search/SearchPresenter.java
+++ app/src/main/java/com/recentgames/screen/search/SearchPresenter.java
@@ -3,12 +3,16 @@ package com.recentgames.screen.search;
 import android.support.annotation.NonNull;
 
 import com.recentgames.R;
+import com.recentgames.model.content.GamePreview;
 import com.recentgames.repository.RepositoryProvider;
+import com.recentgames.router.GamesRouter;
+import com.recentgames.util.RxSchedulers;
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import ru.arturvasilov.rxloader.LifecycleHandler;
+import ru.arturvasilov.rxloader.RxUtils;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -25,16 +29,20 @@ public class SearchPresenter {
     public void searchGame(String name) {
         RepositoryProvider.provideGiantBombRepository()
                 .search(name)
-                .debounce(500, TimeUnit.MILLISECONDS)
-                .doOnSubscribe(mSearchView::showLoading)
-                .doAfterTerminate(mSearchView::hideLoading)
+                .debounce(300, TimeUnit.MILLISECONDS)
                 .compose(mLifecycleHandler.reload(R.id.search_toolbar))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+                .compose(RxSchedulers.async(mSearchView))
                 .subscribe(mSearchView::showGames, throwable -> mSearchView.showError());
     }
 
-    public void clear(){
-        mSearchView.showGames(new ArrayList<>());
+    public void onGameClick(GamesRouter router, GamePreview game) {
+        router.navigateFromSearchToGameDetails(game);
+    }
+
+    public void onTextChanged(String text) {
+        if(text.length() == 0)
+            mSearchView.showGames(new ArrayList<>());
+        else if(text.length() >= 3)
+            searchGame(text);
     }
 }
--- app/src/main/java/com/recentgames/util/RxSchedulers.java
+++ app/src/main/java/com/recentgames/util/RxSchedulers.java
@@ -1,13 +1,17 @@
 package com.recentgames.util;
 
+import com.recentgames.screen.LoadingView;
+
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class RxSchedulers {
 
-    public static <T> Observable.Transformer<T, T> async(){
+    public static <T> Observable.Transformer<T, T> async(LoadingView view){
         return observable -> observable
+                .doOnSubscribe(view::showLoading)
+                .doAfterTerminate(view::hideLoading)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
