dad18d6765c395249608bc383c48efdc5e509c37
Yury <permenko@gmail.com>
2016-09-21 01:10:47 +0300

    [task8] add progressbar, call getGames

--- app/src/main/java/com/recentgames/screen/games/GamesPageFragment.java
+++ app/src/main/java/com/recentgames/screen/games/GamesPageFragment.java
@@ -1,15 +1,14 @@
 package com.recentgames.screen.games;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
-import com.recentgames.GamesType;
 import com.recentgames.R;
 import com.recentgames.model.content.GamePreview;
 import com.recentgames.router.GamesRouter;
@@ -20,18 +19,16 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.Unbinder;
 import ru.arturvasilov.rxloader.LifecycleHandler;
 import ru.arturvasilov.rxloader.LoaderLifecycleHandler;
 
 public class GamesPageFragment extends Fragment implements GamesPageView {
 
-    private Unbinder mUnbinder;
-
-    private boolean wtf = false;
-
+    @BindView(R.id.games_page_progress)
+    ProgressBar mProgressBar;
     @BindView(R.id.games_page_recycler)
     RecyclerView mGamesPageRecyclerView;
+
     protected GamesRouter mGamesRouter;
 
     private GamesPagePresenter mGamesPagePresenter;
@@ -52,30 +49,12 @@ public class GamesPageFragment extends Fragment implements GamesPageView {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        mGamesPagePresenter.getGames(type);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_games_page, container, false);
-        mUnbinder = ButterKnife.bind(this, layout);
+        ButterKnife.bind(this, layout);
         type = getArguments().getInt(KEY_TYPE, TYPE_DEFAULT);
         mGamesRouter = new GamesRouterImpl(getActivity().getSupportFragmentManager());
-        //initRecycler(layout);
 
         mGamesAdapter = new GamesPageAdapter(new ArrayList<>());
         mGamesPageRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
@@ -84,20 +63,9 @@ public class GamesPageFragment extends Fragment implements GamesPageView {
         LifecycleHandler lifecycleHandler = LoaderLifecycleHandler.create(getContext(), getActivity().getSupportLoaderManager());
         mGamesPagePresenter = new GamesPagePresenter(lifecycleHandler, this);
 
-        //mGamesPagePresenter.getGames(type);
-        /*if (wtf) {
-            String wait = "wait";
-        } else {
-            mGamesPagePresenter.getGames(type);
-            String wait = "wait";
-        }*/
-        return layout;
-    }
+        mGamesPagePresenter.getGames(type);
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mUnbinder.unbind();
+        return layout;
     }
 
     @Override
@@ -108,21 +76,22 @@ public class GamesPageFragment extends Fragment implements GamesPageView {
     @Override
     public void updateAdapter(List<GamePreview> gamePreviews) {
         mGamesAdapter.changeDataSet(gamePreviews);
-        wtf = true;
     }
 
     @Override
     public void showError() {
-        String wait = "wait";
+
     }
 
     @Override
     public void showLoading() {
-
+        mProgressBar.setVisibility(View.VISIBLE);
+        mGamesPageRecyclerView.setVisibility(View.GONE);
     }
 
     @Override
     public void hideLoading() {
-
+        mProgressBar.setVisibility(View.GONE);
+        mGamesPageRecyclerView.setVisibility(View.VISIBLE);
     }
 }
