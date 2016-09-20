4df3833cd1ed110923299a0378d33c1a802785d3
Yury <permenko@gmail.com>
2016-09-21 00:54:48 +0300

    [task8] add @IntDef

--- app/src/main/java/com/recentgames/GamesType.java
+++ app/src/main/java/com/recentgames/GamesType.java
@@ -1,6 +1,9 @@
 package com.recentgames;
 
-public interface GamesType {
+import android.support.annotation.IntDef;
+
+@IntDef({GamesType.WEEK, GamesType.MONTH, GamesType.YEAR})
+public @interface GamesType {
     int WEEK = 0;
     int MONTH = 1;
     int YEAR = 2;
